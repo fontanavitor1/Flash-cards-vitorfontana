2e9de5692fe9246f6e3c4ce29072c769e4fb1f28
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2024-10-18 12:26:32 -0300

    18/10/2024

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Geografia' ,
+    'Qual o local mais prufundo do oceano?' ,
+    'Fossa das marianas' ,
+)
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta' ,
+)
+
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta' ,
+)
+
+
+criaCartao(
+    'Categoria' ,
+    'Pergunta?' ,
+    'Resposta' ,
+)
+
+
+
+
+
