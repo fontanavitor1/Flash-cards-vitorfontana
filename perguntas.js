c3785c9d60295eb5c8073d298dd9c905caee5831
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2024-10-25 08:39:51 -0300

    25/10

--- perguntas.js
+++ perguntas.js
@@ -5,23 +5,23 @@ criaCartao(
 )
 
 criaCartao(
-    'Categoria' ,
-    'Pergunta?' ,
-    'Resposta' ,
+    'Anatomia' ,
+    'Quantos pares de costelas um ser humano, normalmente, possui?' ,
+    '12 pares' ,
 )
 
 
 criaCartao(
-    'Categoria' ,
-    'Pergunta?' ,
-    'Resposta' ,
+    'Biologia' ,
+    'Qual animal produz o som mais alto?' ,
+    'BALEIA AZUL' ,
 )
 
 
 criaCartao(
-    'Categoria' ,
-    'Pergunta?' ,
-    'Resposta' ,
+    'Anatomia' ,
+    'Normalmente, quantos litros de sangue uma pessoa tem?' ,
+    '4 a 6 litros' ,
 )
 
 
