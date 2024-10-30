38191bbc70572ce5ff0eccfaa44786fa22aa1e25
fontanavitor1 <145573605+fontanavitor1@users.noreply.github.com>
2024-10-30 09:40:35 -0300

    30/10

--- aap.js
+++ aap.js
@@ -1,10 +1,10 @@
 function criaCartao(categoria, pergunta, resposta){
-    let container=document.getElementById('container')
-    let cartao =document.createElement('article');
-    cartao.className = cartao; 
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
 
-    cartao.innerHTML=`
-                <div class="cartao__conteudo">
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
                         <p>${pergunta}</p>
@@ -14,5 +14,5 @@ function criaCartao(categoria, pergunta, resposta){
                     </div>
                 </div>
     `
-container.appendChild(cartao);
+    container.appendChild(cartao);
 }
